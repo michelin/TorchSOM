084eb26afdf55bfaddd78e143fff3dd0876048db
LouisTier <louis_tier@outlook.com>
2025-03-27 17:59:56 +0100

    Added a comment.

--- TorchSOM/__init__.py
+++ TorchSOM/__init__.py
@@ -4,6 +4,8 @@ from .distances import DISTANCE_FUNCTIONS
 from .neighborhood import NEIGHBORHOOD_FUNCTIONS
 
 __version__ = "0.1.0"
+
+# Define what should be imported when using 'from TorchSOM import *'
 __all__ = [
     "TorchSOM",
     "DISTANCE_FUNCTIONS",
